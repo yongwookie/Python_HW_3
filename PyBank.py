b9d8ce6473d02c444d42ca56b2dded2d9d1b4efe
yongwookie <47338770+yongwookie@users.noreply.github.com>
2019-03-24 23:31:09 -0700

    Add files via upload

--- PyBank.py
+++ PyBank.py
@@ -1,38 +1,38 @@
+#import dependency
 import os
 import csv
 
+#set file path
+csvpath = "03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv"
 
-csvpath = ("03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv")
+#empty list to iterate through rows
+months_list = []
+profit_list = []
+revenue = []
 
-total_months = []
-total_profit = []
-monthly_profit_change = []
+# #open csv
+# matt is an object that python is container all the information inside the csv into
+# julia is actually reading the line by line content inside matt (the container)
+with open(csvpath, newline='') as matt:
 
-with open(csvpath, newline='') as csvfile:
+    julia = csv.reader(matt, delimiter=',')
+# #skip header
+    header = next(julia)
 
-    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in julia:
+          thismonthrevenue = int(row[1])
+          months_list.append(row[0])
+          profit_list.append(int(row[1]))
+          revenue.append(int(row[1]))
 
-    next(csvreader)
-
-    for row in csvreader:
-        total_months.append(row[0])
-        total_profit.append(row[1])
-
-number_of_months = len(total_months)
-
-total_revenue = 0
-i = 0
-
-for i in range(number_of_months):
-    total_revenue = total_revenue + int(total_profit[i])
-
-
-#When calculating the average revenue change the intervals is one less than total months
-average_change_revenue = round(change_revenue/(months-1),2)
-
+number_of_months = len(months_list)
+totalprofit= sum(profit_list)
+total_revenue = len(revenue)
 
 print("Financial Analysis")
-print("--------------")
+print("-------------------")
 print(number_of_months)
+print(totalprofit)
 print(total_revenue)
-print("Average Change + {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}" )
+
+
